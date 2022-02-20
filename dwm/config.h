240af2d9adab7a5d0246e57d3a2bda483f72f620
Nubl1simo <j.goicochea2007@gmail.com>
2022-02-20 16:27:04 +0100

    new dwm config + 1 patch

--- dwm/config.h
+++ dwm/config.h
@@ -9,16 +9,8 @@ static const char dmenufont[]       = "JetBrains Mono:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-//
-//static const char col_gray1[]       = "#eeeeee";
-//static const char col_gray3[]       = "#444444";
-//static const char col_gray2[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-/*static const char col_cyan[]        = "#005577";*/
-//static const char col_cyan[]        = "#d79921"; //the color is actually yellow
-//static const char col_cyan[]        = "#b16286"; //magenta test 1
-//static const char col_cyan[]        = "#d3869b"; //magenta test 1
-static const char col_cyan[]        = "#689d6a"; //magenta test 1
+static const char col_cyan[]        = "#458588";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -26,8 +18,6 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-/*static const char *tags[] = { "Les Terms", "Les Webs", "Le Talk", "Devious Licks", "Angelical Cuddles", "六", "七", "八", "九" };*/
-//static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -37,7 +27,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -48,9 +37,6 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	//{ "[]=",      tile },    /* first entry is default */
-	//{ "><>",      NULL },    /* no layout function means floating behavior */
-	//{ "[M]",      monocle },
 	{ "T=",      tile },    /* first entry is default */
 	{ "F",      NULL },    /* no layout function means floating behavior */
 	{ "M",      monocle },
@@ -71,7 +57,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 
 /*static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf"    , col_gray4, NULL };*/
-static const char *dmenucmd[] = { "/usr/bin/startup.sh", NULL };
+static const char *dmenucmd[] = { ".local/bin/startup.sh", NULL };
 
 
 /*static const char *dmenucmd[] = { "dmenu_run", "-i", "-l" "12", "-p" "Run:" "-sf", "#32302F", "-nb", "#32302F", "-sb", "#8EC07C", "-nf", "#8EC07C", NULL };*/
