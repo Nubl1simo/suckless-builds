0cb810fc009261c3ad9646aa69d76157bbcf5561
Nubl1simo <j.goicochea2007@gmail.com>
2022-03-08 13:11:03 +0100

    fixed emojis

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date '+%b %d (%a) %I:%M%p'",		5,		0},
+	{"🔋", "date '+%b %d (%a) %I:%M%p'",		5,		0},
 	{"", "bat",			  		5,		3},
 //	{"", "pomodoro",				0,		0},
 };
